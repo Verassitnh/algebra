0b120aca39cb5d643ec5b89328851816ecc26ba3
Jackson Mooring <jackson.mooring@gmail.com>
2020-09-17 20:40:05 -0500

    Theme site and setup MathJax

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+# Python version must be either 3.5, 3.6, 3.7, or 3.8
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install mkdocs
+pip install mkdocs
+
+# Install mkdocs-material
+pip install mkdocs-material
